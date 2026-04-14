3e7dea983506ba4a29496655a6414d13e234f322
khushboo <154126439+Khushiprajapati01@users.noreply.github.com>
2026-04-14 23:09:56 +1400

    Refactor password generation script

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ function generatePassword() {
         alert("Please select at least one character type!");
         return;
     }
-
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
